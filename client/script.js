04420394d99ae54560541e0d5a42c6026621892a
papu305 <116421236+papu305@users.noreply.github.com>
2023-02-14 15:20:30 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://cattgpt.onrender.com/',
+    const response = await fetch('https://chatgpt.onrender.com/',
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
